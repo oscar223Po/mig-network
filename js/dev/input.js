7af4a6cf1ac3c77bf2435fd152963f5dc9ccac50
Oscar <oscaritoparapito225@mail.ru>
2025-08-22 20:20:13 +0600

    Updates

--- js/dev/input.js
+++ js/dev/input.js
@@ -1,24 +1,26 @@
-document.querySelectorAll('.file input[type="file"]').forEach((input2) => {
-  input2.addEventListener("change", () => {
-    const fileNameSpan = input2.closest("label").nextElementSibling;
-    if (input2.files.length > 0) {
-      fileNameSpan.textContent = "Вы выбрали: " + input2.files[0].name;
+document.querySelectorAll('.file input[type="file"]').forEach((input) => {
+  input.addEventListener("change", () => {
+    const fileNameSpan = input.closest("label").nextElementSibling;
+    if (input.files.length > 0) {
+      fileNameSpan.textContent = "Вы выбрали: " + input.files[0].name;
     } else {
       fileNameSpan.textContent = "";
     }
   });
 });
-const input = document.querySelector(".search__input");
+const inputSearch = document.querySelector(".search__input");
 const cancelBtn = document.querySelector(".search__button--cancel");
-input.addEventListener("input", () => {
-  if (input.value.trim() !== "") {
-    cancelBtn.classList.add("active-clear");
-  } else {
+if (cancelBtn !== null) {
+  inputSearch.addEventListener("input", () => {
+    if (inputSearch.value.trim() !== "") {
+      cancelBtn.classList.add("active-clear");
+    } else {
+      cancelBtn.classList.remove("active-clear");
+    }
+  });
+  cancelBtn.addEventListener("click", () => {
+    inputSearch.value = "";
     cancelBtn.classList.remove("active-clear");
-  }
-});
-cancelBtn.addEventListener("click", () => {
-  input.value = "";
-  cancelBtn.classList.remove("active-clear");
-  input.focus();
-});
+    inputSearch.focus();
+  });
+}
